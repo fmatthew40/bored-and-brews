18936d72626e0e83b05f680d86f9f12dbbc311f8
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-06 23:56:44 -0700

    completed functionality of checkboxes

--- assets/javascript/bored.js
+++ assets/javascript/bored.js
@@ -1,4 +1,7 @@
-
+var clear = document.getElementById("clear")
+clear.addEventListener("click", function(){
+    location.reload();
+});
 
 var recreational = document.getElementById("recreational");
 
@@ -8,21 +11,149 @@ recreational.addEventListener('change', function() {
             if (response.ok)  {
                 response.json().then(function(data) {
                     console.log(data);
-
-
-
                     var returnedActivities = document.getElementById("returned-activities");
                     var li = document.createElement("li");
                     returnedActivities.appendChild(li);
-                    li.innerText = "hi";
-
-
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
                 });
             }
         });
     }
 });
 
+var cooking = document.getElementById("cooking");
+
+cooking.addEventListener('change', function() {
+    if (cooking.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=cooking").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var education = document.getElementById("education");
+
+education.addEventListener('change', function() {
+    if (education.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var music = document.getElementById("music");
+
+music.addEventListener('change', function() {
+    if (music.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var relaxation = document.getElementById("relaxation");
+
+relaxation.addEventListener('change', function() {
+    if (relaxation.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var diy = document.getElementById("diy");
+
+diy.addEventListener('change', function() {
+    if (diy.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var social = document.getElementById("social");
+
+social.addEventListener('change', function() {
+    if (social.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
+
+var busywork = document.getElementById("busywork");
+
+busywork.addEventListener('change', function() {
+    if (busywork.checked) {
+        fetch("http://www.boredapi.com/api/activity?type=education").then(function(response) {
+            if (response.ok)  {
+                response.json().then(function(data) {
+                    console.log(data);
+                    var returnedActivities = document.getElementById("returned-activities");
+                    var li = document.createElement("li");
+                    returnedActivities.appendChild(li);
+                    var recreationalActivity = data['activity'];
+                    li.innerText = recreationalActivity;
+                });
+            }
+        });
+    }
+});
 
 
    
